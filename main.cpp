7f26f3f0314100ef333d1e602b08a5c914917dd1
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-07-31 14:05:22 +0300

    Defined class interface. Test the class to be constructable.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#include <vector>
 #include "gtest/gtest.h"
+#include "zip.h"
 
 using namespace std;
 
 TEST(Demo, FirstTest) {
     ASSERT_TRUE(true);
+}
+
+TEST(Construct, VectorsInitialization) {
+    vector<int> v1 = {1, 2, 3};
+    vector<char> v2 = {'a', 'b', 'c'};
+    auto z = zip(v1, v2);
+}
+
+TEST(Construct, ConstVectorsInitialization) {
+    const vector<int> v1 = {1, 2, 3};
+    const vector<char> v2 = {'a', 'b', 'c'};
+    auto z = zip(v1, v2);
+}
+
+TEST(Construct, DefaultConstructable) {
+    auto z = zip();
 }
 
 int main(int argc, char** argv) {
--- zip.h
+++ zip.h
@@ -1,3 +1,18 @@
 #pragma once
+#include <tuple>
 
-class zip {};
+template <typename... Types>
+class Zip {
+public:
+    explicit Zip(Types&& ... args);
+};
+
+template<typename... Types>
+Zip<Types...> zip(Types&& ... args) {
+    return Zip<Types...>(std::forward<Types>(args)...);
+}
+
+template<typename... Types>
+Zip<Types...>::Zip(Types&& ... args) {
+    // TODO: store iterators
+}
