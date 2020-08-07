325b9c73edd017f44a1081e73ab0baa20a78b1ce
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2020-08-07 13:58:37 +0300

    Moved ZipIterator methods definitions into the class. Reordered functions. No actual changes.

--- zip.h
+++ zip.h
@@ -46,26 +46,6 @@ namespace zip_impl {
         // Something like tuple<const int&, const int&>. Used as return value of constant version of operator*.
         using const_value_type = std::tuple<typename value_helper<Iters>::const_value...>;
 
-        ZipIterator& operator++();
-
-        template <typename = std::enable_if_t<std::is_convertible_v<iterator_category, std::bidirectional_iterator_tag>, int>>
-        ZipIterator& operator--() {
-            ApplyToIterators([](auto& x){ --x; }, std::index_sequence_for<Iters...>{});
-            return *this;
-        }
-
-        value_type operator*();
-
-        const_value_type operator*() const;
-
-        bool operator==(const ZipIterator& other) const;
-
-        inline bool operator!=(const ZipIterator& other) const {
-            return !operator==(other);
-        }
-
-        inline const Base& AsTuple() const { return *this; }
-
     private:
         template<typename F, size_t... Indexes>
         inline void ApplyToIterators(F&& f, std::integer_sequence<size_t, Indexes...>) {
@@ -92,48 +72,57 @@ namespace zip_impl {
         inline const_value_type CombineValues(std::integer_sequence<size_t, Indexes...>) const {
             return std::forward_as_tuple(*std::get<Indexes>(*this)...);
         }
+
+    public:
+        ZipIterator& operator++() {
+            ApplyToIterators([](auto& x){ ++x; }, std::index_sequence_for<Iters...>{});
+            return *this;
+        }
+
+        template <typename = std::enable_if_t<std::is_convertible_v<iterator_category, std::bidirectional_iterator_tag>, int>>
+        ZipIterator& operator--() {
+            ApplyToIterators([](auto& x){ --x; }, std::index_sequence_for<Iters...>{});
+            return *this;
+        }
+
+        value_type operator*() {
+            return CombineValues(std::index_sequence_for<Iters...>{});
+        }
+
+        const_value_type operator*() const {
+            return CombineValues(std::index_sequence_for<Iters...>{});
+        }
+
+        bool operator==(const ZipIterator& other) const {
+            if (this == &other)
+                return true;
+            // Поскольку при сравнении итераторов, полученных из разных контейнеров,
+            //  поведение программы не определено,то при сравнении двух ZipIterator рассматривается только случай,
+            //  когда оба итератора z1, z2 были получены при помощи корректного вызова класса Zip.
+            // В таком случае возможны следующие сценарии:
+            // 1. Сравниваются два итератора, изначально полученные при помощи метода Zip::begin, и, возможно,
+            //    несколько раз инкрементированные. Если количество инкрементов у z1 и z2 одинаково,
+            //    то все хранимые итераторы на контейнеры совпадают, иначе все хранимые итераторы на контейнера различны.
+            // 2. Сравниваются два итератора, полученные при помощи Zip::end.
+            //    В этом случае все итераторы, хранящиеся в z1 и z2 получены при помощи вызова функции end от контейнера,
+            //    поэтому все хранимые итераторы равны.
+            // 3. Один из итераторов получен при помощи Zip::begin и, возможно, несколько раз инкрементирован,
+            //    другой получен при помощи метода Zip:end.
+            //    Так как итерация по возвращаемому значению zip должна завершиться,
+            //    как только хотя бы один из хранимых итераторов достигает конца диапазона,
+            //    то в этом случае ZipIterator должны считаться равными тогда и только тогда,
+            //    когда хотя бы один из хранимых итераторов в z1 равен соответствующему хранимому итератору z2.
+            // В любом из рассматриваемых случаев достаточно проверить, выполнено ли равенство хранимых итераторов
+            //  хотя бы для одной пары.
+            return AnyEquals(other, std::index_sequence_for<Iters...>{});
+        }
+
+        inline bool operator!=(const ZipIterator& other) const {
+            return !operator==(other);
+        }
+
+        inline const Base& AsTuple() const { return *this; }
     };
-
-    template<typename... Types>
-    ZipIterator<Types...>& ZipIterator<Types...>::operator++() {
-        ApplyToIterators([](auto& x){ ++x; }, std::index_sequence_for<Types...>{});
-        return *this;
-    }
-
-    template<typename... Types>
-    typename ZipIterator<Types...>::value_type ZipIterator<Types...>::operator*() {
-        return CombineValues(std::index_sequence_for<Types...>{});
-    }
-
-    template<typename... Types>
-    typename ZipIterator<Types...>::const_value_type ZipIterator<Types...>::operator*() const {
-        return CombineValues(std::index_sequence_for<Types...>{});
-    }
-
-    template<typename... Types>
-    bool ZipIterator<Types...>::operator==(const ZipIterator& other) const {
-        if (this == &other)
-            return true;
-        // Поскольку при сравнении итераторов, полученных из разных контейнеров,
-        //  поведение программы не определено,то при сравнении двух ZipIterator рассматривается только случай,
-        //  когда оба итератора z1, z2 были получены при помощи корректного вызова класса Zip.
-        // В таком случае возможны следующие сценарии:
-        // 1. Сравниваются два итератора, изначально полученные при помощи метода Zip::begin, и, возможно,
-        //    несколько раз инкрементированные. Если количество инкрементов у z1 и z2 одинаково,
-        //    то все хранимые итераторы на контейнеры совпадают, иначе все хранимые итераторы на контейнера различны.
-        // 2. Сравниваются два итератора, полученные при помощи Zip::end.
-        //    В этом случае все итераторы, хранящиеся в z1 и z2 получены при помощи вызова функции end от контейнера,
-        //    поэтому все хранимые итераторы равны.
-        // 3. Один из итераторов получен при помощи Zip::begin и, возможно, несколько раз инкрементирован,
-        //    другой получен при помощи метода Zip:end.
-        //    Так как итерация по возвращаемому значению zip должна завершиться,
-        //    как только хотя бы один из хранимых итераторов достигает конца диапазона,
-        //    то в этом случае ZipIterator должны считаться равными тогда и только тогда,
-        //    когда хотя бы один из хранимых итераторов в z1 равен соответствующему хранимому итератору z2.
-        // В любом из рассматриваемых случаев достаточно проверить, выполнено ли равенство хранимых итераторов
-        //  хотя бы для одной пары.
-        return AnyEquals(other, std::index_sequence_for<Types...>{});
-    }
 
     template<typename... Types>
     class Zip {
